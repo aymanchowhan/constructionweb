817df1493b3fecc6e25a35058d7917013b5e733e
Ayman Chowhan <aymanchowhan@gmail.com>
2024-06-04 03:24:01 +0530

    we are

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
             <div id='home' className=' container m-auto flex h-full px-[10%]'>
                 <div className='m-auto lg:justify-center'>
                     <div className='text-center lg:text-lef text-[35px] lg:text-[70px] lg:mt-36 text-white'>
-                        We`&apos;`re Building Your Future
+                        We&apos;re Building Your Future
                         <div className='text-center text-[14px] lg:text-[18px] mt-2'>
                             Take a step towards the future of construction with Cobuild
                             <div className='flex justify-center lg:mt-16 mt-7 '>
